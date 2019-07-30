68f268aa1097511474b2b5e4130ba35ee8b62e6a
pengyan1234 <15859204837@163.com>
2019-07-30 16:47:39 +0800

    iview项目

---
+++ iview-admin-master/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <LoginForm></LoginForm>
+  </div>
+</template>
+
+<script>
+import LoginForm from '_c/login-form'
+export default {
+  name: 'App',
+    components: {
+    LoginForm
+  },
+}
+</script>
+
+<style lang="less">
+.size{
+  width: 100%;
+  height: 100%;
+}
+html,body{
+  .size;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  .size;
+}
+</style>
